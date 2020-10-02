e46f78088636d543281548a28315074d793fa967
Salocin1987 <72270336+Salocin1987@users.noreply.github.com>
2020-10-02 19:27:41 +0200

    Add files via upload

---
+++ data/centredintrts_1.js
@@ -0,0 +1,8 @@
+var json_centredintrts_1 = {
+"type": "FeatureCollection",
+"name": "centredintrts_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ -90.118878087370433, 29.920972969885185 ] } }
+]
+}
